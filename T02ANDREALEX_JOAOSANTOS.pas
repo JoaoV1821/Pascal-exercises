0b8fa69d220fbf9c50c7bd266fcba6bd050e2dc4
JV <joaovitoraraujo1821@gmail.com>
2022-07-12 21:26:19 -0300

    Implementei o leia1

--- T02ANDREALEX_JOAOSANTOS.pas
+++ T02ANDREALEX_JOAOSANTOS.pas
@@ -6,74 +6,67 @@ var nota, soma, mediaInt, qtdNotas : integer;
 var mediaReal : real;
 var msg: string;
 
-{ LEIA1 eH um procedimento que informa o que deve ser digitado (oque) e faz a leitura de informações do
-teclado como caractere (max 30- - string[30]) e devolve o valor como inteiro (int) }
+procedure leia1 (msg: string[30]; var inter:integer);
 
+var dm: string[20];
+e: integer ;
 
-// Construção da procedure leia1
+begin
+  repeat
+    writeln(msg, '=>');
+    readln(dm);
+    val(dm, inter, e);
+    
+    if(e > 0) then
+    writeln('Valor ilegal');
+  until (e=0);
+end;
 
-// ================================================
-{procedure leia1 (var msg: string[30]; inte: integer);
-	var dm : string[20];
-		 	e: integer;
-	begin
-	repeat
-		write(msg, '=> ');
-		readln(dm);	
-		val(dm, inte, e);
-		if (e>0) then
-			writeln(msg, 'ilegal');
-		until(e=0);
-	end;}
-// =================================================
-
-// Programa principal	
+// Programa principal
 Begin // 1
-	qtdNotas := 0;
-	soma := 0;
-	
-	msg := 'Digite a nota: ';
-			
-	while(nota >= 0) do
-		begin //2
-		
-			writeln('Digite a nota: ');
-			readln(nota);
-					
-			if (nota > 100) then
-				repeat
-					writeln('ilegal');
-					writeln('Digite a nota');
-					readln(nota)
-				until (nota <= 100)
-						
-			else
-				if(nota >= 0) then
-					begin // 3 
-						soma := soma + nota;
-						qtdNotas := qtdNotas + 1;
-					end  // 3
-					
-			end; //2
-	
-	if (qtdNotas <> 0) then
-		begin //4
-		
-			mediaInt := soma DIV qtdNotas;
-			mediaReal := soma / qtdNotas;
-			
-		end  //4
-		
-	else 
-		begin //5
-		
-			mediaInt := 0;
-		 	mediaReal := 0;
-		 	
-		end; //5
-		
-	writeln('Média inteira: ', mediaInt:2, 'Média real: ', mediaReal:2 );
-	writeln('Pressione ENTER para sair');
-	readln();
-	
+  qtdNotas := 0;
+  soma := 0;
+  
+  msg := 'Digite a nota: ';
+  
+  while(nota >= 0) do
+  begin //2
+    
+    leia1('Digite a nota', nota);
+    
+    if (nota > 100) then
+    repeat
+      writeln('ilegal');
+      leia1('Digite a nota', nota)
+    until (nota <= 100);
+    
+    if (nota >= 0) then
+    begin //3
+      soma := soma + nota;
+      qtdNotas := qtdNotas + 1;
+    end  //3
+    
+    
+  end; //2
+  
+  if (qtdNotas <> 0) then
+  begin //4
+    
+    mediaInt := soma DIV qtdNotas;
+    mediaReal := soma / qtdNotas;
+    
+  end  //4
+  
+  else
+  begin //5
+    
+    mediaInt := 0;
+    mediaReal := 0;
+    
+  end; //5
+  
+  writeln('Média inteira: ', mediaInt:2, ' Média real: ', mediaReal:2, ' Quantidade de notas lidas: ', qtdNotas );
+  writeln('Pressione ENTER para sair');
+  readln();
+  
 End.
--- T03ANDREALEX_JOAOSANTOS.pas
+++ T03ANDREALEX_JOAOSANTOS.pas
@@ -1,76 +1,90 @@
 Program T03ANDREALEX_JOAOSANTOS;
 {
-	André Alex Jankoski
-	João Vitor Araújo dos Santos
+André Alex Jankoski
+João Vitor Araújo dos Santos
 }
 
 var dias ,mes, ano : integer;
 
-function validaDias(mes, ano : integer):integer;
-	var dias : integer;
+procedure leia1 (msg: string[30]; var inter:integer);
+
+	var dm: string[20];
+	e: integer ;
 	
 	begin
-		if ((mes = 4) or (mes = 5) or (mes = 9) or (mes = 11) ) then
-			dias := 30
-		else 
-			dias := 31;
-		
-		if (mes = 2) then
-			begin
-				if (ano MOD 4 = 0) then
-					dias := 29
-				else 
-					dias := 28;
-			end;
-			
-			validaDias := dias;
+	  repeat
+	    writeln(msg, '=>');
+	    readln(dm);
+	    val(dm, inter, e);
+	    
+	    if(e > 0) then
+	    writeln('Valor ilegal');
+	  until (e=0);
 	end;
+
+function validaDias(mes, ano : integer):integer;
+var dias : integer;
+
+begin
+  if ((mes = 4) or (mes = 5) or (mes = 9) or (mes = 11) ) then
+  dias := 30
+  else
+  dias := 31;
+  
+  if (mes = 2) then
+  begin
+    if (ano MOD 4 = 0) then
+    dias := 29
+    else
+    dias := 28;
+  end;
+  
+  validaDias := dias;
+end;
 
 function validaData(dia, mes, ano : integer) : boolean;
-	var chave : boolean;
-	var dias : integer;
-	
-	begin
-		chave := true;
-		
-		if((ano < 1950) or (ano > 3000)) then
-			chave := false
-		else 
-			if ((mes < 1) or (mes > 12))then
-				chave := false
-				
-				else
-					begin
-						dias := validaDias(mes, ano);
-						
-						if((dia > dias) or (dia < 1)) then
-							chave := false
-					end; 
-						
-		validaData := chave;
-		
-	end;
+var chave : boolean;
+var dias : integer;
+
+begin
+  chave := true;
+  
+  if((ano < 1950) or (ano > 3000)) then
+  chave := false
+  else
+  if ((mes < 1) or (mes > 12))then
+  chave := false
+  
+  else
+  begin
+    dias := validaDias(mes, ano);
+    
+    if((dia > dias) or (dia < 1)) then
+    chave := false
+  end;
+  
+  validaData := chave;
+  
+end;
 
 Begin
-	write('Digite o dia: ');
-	readln(dias);
-  write('Digite o mês: ');
-  readln(mes);
-  write('Digite o ano: ');
-  readln(ano);
+  
+  leia1('Digite o dia', dias);
+  leia1('Digite o mes', mes);
+  leia1('Digite o ano', ano);
   
   if (validaData(dias, mes, ano)) then
-  	begin
-	  	 writeln(dias : 1, '/', mes:1, '/', ano:1);
-	  	 writeln('Data válida')
-  	end
-  	
+  begin
+    writeln(dias : 1, '/', mes:1, '/', ano:1);
+    writeln('Data válida')
+  end
+  
   else
-  		begin
-	  		writeln(dias : 1, '/', mes:1, '/', ano:1);
-	  		writeln('Data inválida');
-  		end;
-  		
+  begin
+    writeln(dias : 1, '/', mes:1, '/', ano:1);
+    writeln('Data inválida');
+  end;
+  
   write('Digite ENTER para sair');
   readln();
 End.
